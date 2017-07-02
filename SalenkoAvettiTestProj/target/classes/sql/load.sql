386e7aee7da11c33dcbdb0ec6b2ab162d6997283
TheDekan <ssalenkodd@gmail.com>
2017-07-02 12:32:13 +0300

    Delete load.sql

--- SalenkoAvettiTestProj/target/classes/sql/load.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO test_table (parameter_text, parameter_int) VALUES ('shirt', 111);
-INSERT INTO test_table (parameter_text, parameter_int) VALUES ('boots', 222);
-INSERT INTO test_table (parameter_text, parameter_int) VALUES ('gloves', 333);
